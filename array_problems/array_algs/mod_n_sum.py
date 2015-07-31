474ead30cd8ca3cfc8ac839b7674dac50f51e532
Kendrick Tang <kentangsrice@gmail.com>
2015-07-31 15:55:32 -0700

    made suggested changes to mod_n_sum

--- array_problems/array_algs/mod_n_sum.py
+++ array_problems/array_algs/mod_n_sum.py
@@ -1,26 +1,35 @@
-import numpy as np
-
-test = True #
+from collections import defaultdict
 
 def mod_n_sum(A):
-    n=len(A)
-    if n:
-        mod_n_sum = A[0] % n
-        mod_n={}
-        mod_n[mod_n_sum] = [0]
+    """Returns a 2-tuple (a,b) where sum(A[a:b]) == 0 mod n."""
+    if not A:
+        return A
 
-        # Bin i into mod_n_sum[[sum mod n of A[0:i]]
-        for i in xrange(1,n):
-            mod_n_sum = (mod_n_sum + A[i]) % n
-            mod_n.setdefault(mod_n_sum,[])
-            mod_n[mod_n_sum].append(i)
+    # Bin i into mod_n_sum[[sum mod n of A[0:i]]
+    n = len(A)
+    mod_n = defaultdict(list, [])
+    partial_sums = partial_sum_mod_n(A)
+    i = 0
+    while i < n:
+        mod_n_sum = partial_sums.next()
 
-        # If there is a sum mod n which is zero, that is the answer.
-        if mod_n.has_key(0):
-            return np.array(xrange(mod_n[0][0]+1))
+        # Early return for sum A[0:i] == 0 mod n
+        if mod_n_sum == 0:
+            return (0, i + 1)
 
-        # Find i and j in the same bin and subtract to achieve 0 mod n
-        for i in xrange(1, len(A)):
-            if mod_n.has_key(i) and len(mod_n[i]) > 1:
-                return np.array(xrange(mod_n[i][0]+1,mod_n[i][1]+1))
-    return A
+        # Early return for sum A[0:i] == A[0:j] mod n.
+        if mod_n_sum in mod_n:
+            return (mod_n[mod_n_sum][0] + 1, i + 1)
+
+        mod_n[mod_n_sum].append(i)
+        i += 1
+
+
+def partial_sum_mod_n(A):
+    n = len(A)
+    i = 0
+    sum = 0
+    while i < n:
+        sum = (sum + A[i]) % n
+        yield sum
+        i += 1
